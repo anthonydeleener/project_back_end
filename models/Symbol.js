868ed51d8a32048b0a382b0778f643b91a22c311
Chih Huai LIN <chihhuai.lin@student.vinci.be>
2020-11-30 16:58:47 +0100

    ajout class js

---
+++ models/Symbol.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//chaque symbol est un bouton
+
+class Symbol
+{
+    constructor()
+    {
+        let id;
+        let nom;
+        let lienImage;
+    }
+
+    //pour comparaison
+    getID(){
+        return id;
+    }
+}
+
+module.exports.Symbol;
